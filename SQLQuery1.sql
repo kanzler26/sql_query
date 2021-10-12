77e650a19e3168980b6dab95ffbec7da363bb0b6
kanzler26 <39521261+kanzler26@users.noreply.github.com>
2021-10-13 00:01:19 +0300

    сам запрос

---
+++ SQLQuery1.sql
@@ -0,0 +1,21 @@
+﻿Create Table [dbo].[Customers]
+(
+[ID] int Identity Primary Key,
+[NAME] VARCHAR(30) 
+)
+Insert Into Customers (NAME)
+Values
+('Max'),('Pavel'),('Ivan'),('Leonid')
+
+Create Table [dbo].[Orders]
+(
+[ID] int Identity Primary Key,
+[CustomersId] int References Customers(ID) 
+)
+Insert Into Orders (CustomersId)
+Values
+(2),(4)
+Select [Name] From Customers as cust Left Join
+Orders as ord On cust.ID = ord.CustomersId 
+Where
+ord.CustomersId is null
